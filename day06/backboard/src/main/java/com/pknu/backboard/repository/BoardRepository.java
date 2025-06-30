91e2fad544c6b35561e683719d29693a65fc1f19
jungmin9911 <jmahn1819@gmail.com>
2025-06-30 17:31:18 +0900

    0630

---
+++ day06/backboard/src/main/java/com/pknu/backboard/repository/BoardRepository.java
@@ -0,0 +1,12 @@
+package com.pknu.backboard.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pknu.backboard.entity.Board;
+import java.util.List;
+
+
+public interface BoardRepository extends JpaRepository<Board, Long>{    // Board 뒤에 PK 값
+    Board findByTitle(String title); // 제목으로 검색
+    List<Board> findByTitleLike(String title);  // 비슷한 제목으로 검색
+}
